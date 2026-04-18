f6f681d99d5cac496f9adf2a83d5fd6c57e4fcbd
Johannes <jojurg@ttu.ee>
2026-04-18 13:38:34 +0300

    changes in the navigation error file

--- src/errors/navigation.rs
+++ src/errors/navigation.rs
@@ -3,16 +3,16 @@ use std::fmt;
 pub enum NavigationError {
     RowInsertFormNavigation(String),
     RowDeleteFormNavigation(String),
+    #[allow(dead_code)]
     NewTableFormNavigation(String),
 }
 
 impl fmt::Display for NavigationError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "error while navigating ");
         match self {
-            Self::RowInsertFormNavigation(msg) => write!(f, "row insert form: {}", msg),
-            Self::RowDeleteFormNavigation(msg) => write!(f, "row delete form: {}", msg),
-            Self::NewTableFormNavigation(msg) => write!(f, "new table form: {}", msg),
+            Self::RowInsertFormNavigation(msg) => write!(f, "row insert form navigation error: {}", msg),
+            Self::RowDeleteFormNavigation(msg) => write!(f, "row delete form navigation error: {}", msg),
+            Self::NewTableFormNavigation(msg) => write!(f, "new table form navigation error: {}", msg),
         }
     }
 }
